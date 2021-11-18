e37584080378c42ce2d41b26da885dcc12d62e03
Álvaro Barbosa <alvaro.barbosa@empresta.com.br>
2021-11-18 17:33:04 -0300

    identando corretamente o codigo

--- app/BO/VoosBO.php
+++ app/BO/VoosBO.php
@@ -19,7 +19,7 @@ class VoosBO
 
     public function agruparVoos()
     {
-        $this->voos = collect($this->listarVoos());
+        $this->voos                   = collect($this->listarVoos());
         $this->voosAgrupadosPorTarifa = $this->voos->groupBy('fare');
 
         return $this->agruparVoosPorTipoVooEPreco();
@@ -27,34 +27,34 @@ class VoosBO
 
     private function agruparVoosPorTipoVooEPreco()
     {
-        $grupos         = collect();
-        $maisBarato=(object)['idGrupo'=>null,'preco'=>999];
+        $grupos     = collect();
+        $maisBarato = (object)['idGrupo'=>null,'preco'=>999];
 
         $value = null;
         $this->voosAgrupadosPorTarifa->each(
             function($voos) use($grupos, $maisBarato)
             {
-                $voosVolta  =  $voos->where('inbound',1)->groupBy('price');
-                $voosIda    =  $voos->where('outbound',1)->groupBy('price');
+                $voosVolta = $voos->where('inbound',1)->groupBy('price');
+                $voosIda   = $voos->where('outbound',1)->groupBy('price');
 
-                $voosVolta->map(function($voosVolta) use ($grupos,$voosIda,$maisBarato)
+                $voosVolta->map(function($voosVolta) use ($grupos, $voosIda, $maisBarato)
                 {
-                    $voosIda->map(function($voosIda) use ($grupos,$voosVolta,$maisBarato)
+                    $voosIda->map(function($voosIda) use ($grupos, $voosVolta, $maisBarato)
                     {
-                        $id=uniqid();
-                        $total = $voosVolta->first()->price+$voosIda->first()->price;
+                        $id = uniqid();
+                        $total = $voosVolta->first()->price + $voosIda->first()->price;
 
                         $grupos->add([
-                            'uniqueId'=>$id,
+                            'uniqueId'  =>$id,
                             'totalPrice'=>$total,
-                            'inbounds'=>$voosVolta,
-                            'outbounds'=>$voosIda
+                            'inbounds'  =>$voosVolta,
+                            'outbounds' =>$voosIda
                         ]);
 
                         if($maisBarato->preco > $total)
                         {
-                            $maisBarato->idGrupo    =   $id;
-                            $maisBarato->preco      =   $total;
+                            $maisBarato->idGrupo = $id;
+                            $maisBarato->preco   = $total;
                         }
                     });
                 });
@@ -62,12 +62,12 @@ class VoosBO
         );
 
         return [
-            'flights'=>$this->voos,
-            'groups'=>$grupos->sortBy('totalPrice'),
-            'totalGroups'=>$grupos->count(),
-            'totalFlights'=>$this->voos->count(),
-            'cheapestPrice'=>$maisBarato->preco,
-            'cheapestGroup'=>$maisBarato->idGrupo,
+            'flights'       =>$this->voos,
+            'groups'        =>$grupos->sortBy('totalPrice'),
+            'totalGroups'   =>$grupos->count(),
+            'totalFlights'  =>$this->voos->count(),
+            'cheapestPrice' =>$maisBarato->preco,
+            'cheapestGroup' =>$maisBarato->idGrupo,
         ];
     }
 }
